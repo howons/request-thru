7ba9cbbd949598fbf53725f2f167deb47c35f023
howon <shinhw371@naver.com>
2025-04-19 08:45:59 +0900

    style: 로고 이미지 변경

--- src/popup/components/PopupHeader/PopupHeader.tsx
+++ src/popup/components/PopupHeader/PopupHeader.tsx
@@ -1,16 +1,15 @@
 import { ReactElement, ReactNode } from 'react';
 
-import ExtensionRoundedIcon from '@mui/icons-material/ExtensionRounded';
 import { Toolbar } from '@mui/material';
 
 import './PopupHeader.css';
 
 export default function PopupHeader({ children }: { children?: ReactNode }): ReactElement {
-    return (
-        <Toolbar className="popup-header" sx={{ boxShadow: 1 }}>
-            <ExtensionRoundedIcon className="popup-logo" />
-            <h1>Request thru</h1>
-            {children}
-        </Toolbar>
-    );
+  return (
+    <Toolbar className="popup-header" sx={{ boxShadow: 1 }}>
+      <img className="popup-logo" src="/assets/icons/icon-32.png" alt="logo" />
+      <h1>Request thru</h1>
+      {children}
+    </Toolbar>
+  );
 }
